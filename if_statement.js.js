1bc5956f3b1278905f54e9de68490b3052c58de9
ShakherGurjar <shakhergurjar@gmail.com>
2022-09-30 11:47:36 +0530

    first commit

---
+++ if_statement.js.js
@@ -0,0 +1,8 @@
+let a=prompt("whats your age");
+a=number.parseint(a);//converting string into number
+if(age<18){
+           alert("this is valid age for driving")
+          }
+else(age>18){
+	alert("this is not a valid age for driving")
+}
